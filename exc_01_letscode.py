652b85c3eaf364af6f17641bde91950c91788472
Árion Ferraz <arionferraz@gmail.com>
2022-09-15 00:19:28 -0300

    arquivos renomeados

---
+++ exc_01_letscode.py
@@ -0,0 +1,7 @@
+'''                  Exercícios Módulo 1 - Python
+    1. Faça um programa que peça um valor monetário e diminua-o em 15%. 
+       Seu programa deve imprimir a mensagem “O novo valor é [valor]”
+'''
+
+voriginal = float(input("Digite o valor monetário inicial: "))
+print("O novo valor é ", voriginal*0.85)
