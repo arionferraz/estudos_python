39641c05e060701bc769273d6ff06540ab023504
Árion Ferraz <arionferraz@gmail.com>
2022-09-15 01:28:59 -0300

    commit exercicio 4

---
+++ exc_04_letscode.py
@@ -0,0 +1,11 @@
+'''
+    4. Faça um programa que imprima a tabuada do 9 (de 9*1 a 9*10) usando loops.
+'''
+
+cont = 1
+print("Tabuada do nove:")
+print("")
+while cont <= 10:
+    print('9 x ' + str(cont) + ' = ' + str(cont*9))
+    #print(' =', cont*9)
+    cont+=1
